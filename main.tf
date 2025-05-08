6ed05880dd4f240076f87229a5e2fa6b52c67887
harshit410 <harshit.gupta@hashicorp.com>
2025-05-08 11:19:28 +0530

    creating the initial file

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "increase_resource_count" {
+  for_each = { for idx in range(1, 12) : idx => idx }
+
+  prefix = "hello-world-${each.key}"
+  length = 2
+}
+
+output "pet_names" {
+  value = { for k, v in random_pet.increase_resource_count : k => v.id }
+}
