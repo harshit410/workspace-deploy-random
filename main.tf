6c153636da7fbf77b1339a0c682a7ea9be5c34fa
Harshit Gupta <harshit.gupta@hashicorp.com>
2025-06-03 15:01:20 +0530

    number of resources to 8

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "random" {}
 
 resource "random_pet" "increase_resource_count" {
-  for_each = { for idx in range(1, 5) : idx => idx }
+  for_each = { for idx in range(1, 8) : idx => idx }
 
   prefix = "hello-world-${each.key}"
   length = 2
